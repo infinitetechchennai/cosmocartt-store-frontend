952380a3aba3aa1707b3695da568f38dfc38f657
Karthik Krishna <karthikkrishna3666@gmail.com>
2026-06-17 16:40:49 +0530

    Homepage UI redesign and backcase brands improvements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
 import Categories from "../components/Categories";
 import FeaturedProducts from "../components/FeaturedProducts";
 import Brands from "../components/Brands";
@@ -16,48 +15,113 @@ import DealBanner from "../components/DealBanner";
 import FeaturedCategories from "../components/FeaturedCategories";
 import TopDeals from "../components/TopDeals";
 import FeaturesStrip from "../components/FeaturesStrip";
+import TrendingProducts from "../components/TrendingProducts";
+import CategoryShowcase from "../components/CategoryShowcase";
+import "swiper/css/navigation";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Autoplay,
+  Pagination,
+  EffectFade,
+  Navigation,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+
+import mobileBanner from "../assets/mobile-case-banner.jpg";
+import tvBanner from "../assets/tv-remote-banner.jpg";
+import acBanner from "../assets/ac-remote-banner.jpg";
 
 export default function Home() {
-    return (
-        <div className="min-h-screen bg-slate-50">
+  return (
+    <div className="min-h-screen bg-slate-50">
 
-            <AnnouncementBar />
+      <AnnouncementBar />
 
-            <Navbar />
+      <Navbar />
 
-            <CategoryNavbar />
+      <CategoryNavbar />
 
-            <QuickLinks />
+      <QuickLinks />
 
-            <Hero />
+      {/* HERO SLIDER */}
+<div className="max-w-7xl mx-auto px-4 mt-4 relative">
 
-            <FeaturesStrip />
+  <Swiper
+    modules={[
+      Autoplay,
+      Pagination,
+      EffectFade,
+      Navigation,
+    ]}
+    effect="fade"
+    navigation={true}
+    autoplay={{
+      delay: 4000,
+      disableOnInteraction: false,
+    }}
+    pagination={{
+      clickable: true,
+    }}
+    loop={true}
+    grabCursor={true}
+    className="rounded-3xl overflow-hidden shadow-2xl home-swiper"
+  >
+    <SwiperSlide>
+      <img
+        src={mobileBanner}
+        alt="Mobile Cases"
+        className="w-full h-[500px] object-cover"
+      />
+    </SwiperSlide>
 
+    <SwiperSlide>
+      <img
+        src={tvBanner}
+        alt="TV Remote"
+        className="w-full h-[500px] object-cover"
+      />
+    </SwiperSlide>
 
-            <FeaturedCategories />
+    <SwiperSlide>
+      <img
+        src={acBanner}
+        alt="AC Remote"
+        className="w-full h-[500px] object-cover"
+      />
+    </SwiperSlide>
+  </Swiper>
 
-            {/* <Categories /> */}
+</div>
 
-            <Stats />
+      {/* <Categories /> */}
 
-            <WhyChooseUs />
+      <TrendingProducts />
+      
+      <Stats />
 
-            <PremiumBanner />
+<CategoryShowcase />
 
-            <FeaturedProducts />
+<WhyChooseUs />
 
-            <TopDeals />
+      <PremiumBanner />
 
-            <DealBanner />
+      <FeaturedProducts />
 
-            <Brands />
+      <TopDeals />
 
-            {/* <Testimonials /> */}
+      <DealBanner />
 
-            <Newsletter />
+      <Brands />
 
-            <Footer />
+      {/* <Testimonials /> */}
 
-        </div>
-    );
+      <Newsletter />
+
+      <Footer />
+
+    </div>
+  );
 }
